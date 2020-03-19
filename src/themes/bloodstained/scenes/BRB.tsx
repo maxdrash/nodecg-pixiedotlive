6ee98135f6ced7dad36e25ea1cd6061cf22bff7e
dani quinn sexton <spells@witch.zone>
2020-03-19 22:07:41 +0000

    Bloodstained brb fixes

--- src/themes/bloodstained/scenes/BRB.tsx
+++ src/themes/bloodstained/scenes/BRB.tsx
@@ -1,19 +1,34 @@
 import { FunctionComponent, h } from 'preact'
+import { useSelector } from 'react-redux'
 
 import Scene, { Layer } from '../../../components/Scene'
 
-import dani from '../../../assets/themes/bloodstained/dani.webm'
+import { BRBStatus } from 'nodecg-twitchie-graphics'
+import box from '../../../assets/themes/bloodstained/brb-ui.png'
+import dani from '../../../assets/themes/bloodstained/dani-2.webm'
 
-const BRB: FunctionComponent = () => (
-  <Scene showWhenAway className="c-brb-scene">
-    <Layer className="c-brb-scene__background" />
+const BRB: FunctionComponent = () => {
+  const brbStatus: BRBStatus = useSelector((state: any) => state.brb)
 
-    <Layer className="c-brb-scene__dani">
-      <video autoPlay muted loop className="c-dani">
-        <source src={dani} type="video/webm" />
-      </video>
-    </Layer>
-  </Scene>
-)
+  return (
+    <Scene showWhenAway className="c-brb-scene">
+      <Layer className="c-brb-scene__background" />
+
+      <Layer>
+        <img src={box} />
+      </Layer>
+
+      <Layer>
+        <div className="c-brb-scene__message">{brbStatus.message}</div>
+      </Layer>
+
+      <Layer className="c-brb-scene__dani">
+        <video autoPlay muted loop className="c-dani">
+          <source src={dani} type="video/webm" />
+        </video>
+      </Layer>
+    </Scene>
+  )
+}
 
 export default BRB
