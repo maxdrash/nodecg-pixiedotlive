b8bdbd7e6d88ac2eb64bac16746df60abffa89e5
dani quinn sexton <spells@witch.zone>
2020-04-07 17:39:15 +0100

    Tweak mako bubbles

--- src/themes/ff7/components/Mako.tsx
+++ src/themes/ff7/components/Mako.tsx
@@ -4,34 +4,26 @@ import { useEffect, useRef } from 'preact/hooks'
 import 'particles.js'
 
 const config = {
-  canvas: {
-    w: 1280,
-    h: 720,
-  },
   particles: {
     number: {
-      value: 120,
+      value: 180,
       density: {
         enable: true,
-        value_area: 800,
+        value_area: 1000,
       },
     },
     color: {
-      value: '#7ee6c0',
+      value: ['#7ee6c0', '#48ff9e', '#40de80', '#6dffc5', '#38bf76', '#6cffcd'],
     },
     shape: {
       type: 'circle',
-      stroke: {
-        width: 0,
-        color: '#7ee6c0',
-      },
     },
     opacity: {
       value: 0.75,
       random: true,
       anim: {
         enable: true,
-        speed: 0.25,
+        speed: 0.33,
         opacity_min: 0,
         sync: false,
       },
@@ -52,8 +44,8 @@ const config = {
     move: {
       enable: true,
       speed: 0.5,
-      direction: 'none',
-      random: false,
+      direction: 'top',
+      random: true,
       straight: false,
       out_mode: 'out',
       bounce: false,
@@ -72,7 +64,11 @@ const Mako = () => {
     ;(window as any).particlesJS(divRef.current!.id, config)
   }, [])
 
-  return <div id="mako" className="c-mako" ref={divRef} />
+  return (
+    <div className="c-mako">
+      <div className="c-mako__particles" ref={divRef} id="mako" />
+    </div>
+  )
 }
 
 export default Mako
